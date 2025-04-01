9a4704d72cac4f45f8b1f37c2ebabf3cf71f317f
Hrishikesh Alabnur <alabnur2004@gmail.com>
2025-04-02 02:14:43 +0530

    UI update

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -3,10 +3,13 @@ import { useNavigate } from "react-router-dom";
 export default function Landingpage() {
     const navigate = useNavigate();
     return (
-            <div style={{backgroundColor:"#BEB0D2", height:"100vh" , width : "100vw" , display:"flex", justifyContent:"center" , alignItems:"center" }}>
-                <div style={{backgroundColor:"#fff" , height:"83vh" , width:"90vw", borderRadius:"20px" }}>
-                    <button onClick={() => navigate("/upload")}>Get Started</button>
+        <div style={{ backgroundColor: "#fff" , height: "calc(100vh - 30px)", width: "calc(100vw - 30px)", padding: "15px"}}>
+            <div style={{ backgroundColor: "#BD94F0", height: "calc(100% - 150px)", width: "calc(100% - 150px)", padding: "75px" ,borderRadius: "20px"}}>
+                <div style={{ backgroundColor: "#fff", height: "100%", width: "100%", borderRadius: "20px" , position:"relative"}}>
+                    <img src=""/>
+                    <button style={{position:"absolute",bottom:"0px"}} onClick={() => navigate("/upload")}>Get Started</button>
                 </div>
             </div>
+        </div>
     )
 }
